6d5563d285edcc8cffac273271e4f429156cc1e8
Robin Ellingsen <robin-elli@hotmail.com>
2024-11-28 15:28:51 +0100

    Refactor logPrefix in activateBlocks and deactivateBlocks for consistency

--- src/functions/deactivateBlocks.js
+++ src/functions/deactivateBlocks.js
@@ -1,5 +1,5 @@
 const { app } = require('@azure/functions');
-const { misc } = require('../../config.js')
+const { misc, mongoDB } = require('../../config.js')
 const { logger } = require('@vtfk/logger')
 const { handleUserActions } = require('../lib/jobs/handleUserActions.js')
 const { moveDocuments } = require('../lib/jobs/moveDocuments.js')
@@ -9,7 +9,7 @@ app.timer('deactivateBlocks', {
     schedule: '2-59/5 * * * *', 
     // schedule: '*/5 6-21 * * 1-5', // Every 5 minutes between 6am and 9pm, Monday to Friday
     handler: async (myTimer, context) => {
-        const logPrefix = 'deactivateBlock-dev'
+        const logPrefix = 'deactivateBlock'
         console.log(misc.email_domain)
         try {
             const response = await handleUserActions('deactivate')
